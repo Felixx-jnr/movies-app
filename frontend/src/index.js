9058ecc0b15756e8f872c169e7ecd81302e92214
Felixx-jnr <uyuoukoh@gmail.com>
2024-03-04 20:47:37 +0100

    frontend setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Import BrowserRouter and Routes
 
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render
-(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index={true} element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import store from './redux/store'
+// import { Provider } from 'react-redux';
+// import {Route, RouterProvider, Routes, createRoutesFromElements} from 'react-router-dom'
+// import {BrowserRouter} from 'react-router-dom'
+
+// //AUTH
+
+
+// //RESTRICTED
+
+// import Home from './pages/Home'
+
+// const router = (
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />}>
+//         <Route index={true} element={<Home />} /> {/* Remove the path prop from the nested Route */}
+//       </Route>
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Provider store={store}>
+//     <RouterProvider router = {router} />
+//   </Provider>
+// );
+
