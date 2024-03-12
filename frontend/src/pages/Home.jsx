237e2a7efc1733fde19a84fc42df34cd69a2f7ad
Felixx-jnr <uyuoukoh@gmail.com>
2024-03-12 17:56:15 +0100

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import Header from "./Movies/Header";
+import MoviesContainerPage from "./Movies/MoviesContainer";
+
 const Home = () => {
-  return ( 
-    <div>Home</div>
-   );
-}
- 
+  return (
+    <>
+      <Header />
+
+      <section className="mt-[10rem]">
+        <MoviesContainerPage />
+      </section>
+    </>
+  );
+};
+
 export default Home;
