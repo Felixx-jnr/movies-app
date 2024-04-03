ec0e1fa3e1dca629a81dddb7ec927123cd37fb38
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-03 15:09:47 +0100

    done

--- frontend/src/components/ImageSlider.jsx
+++ frontend/src/components/ImageSlider.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import picOne from "../assets/1.jpg";
 import picTwo from "../assets/2.jpg";
