ab755b6cf900b77b13f3609d25112d6a8d2efcb3
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-05 00:40:21 +0100

    slice

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,28 @@
+// import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+// import { BASE_URL } from "../constants";
+
+// const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+// export const apiSlice = createApi({
+//   baseQuery,
+//   endpoints: () => ({}),
+// });
+
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
