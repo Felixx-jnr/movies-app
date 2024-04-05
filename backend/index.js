cc70388ffde81c5a93114eaec2d8a91f143c88d3
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-05 12:43:57 +0100

    alloriginallowed

--- backend/index.js
+++ backend/index.js
@@ -14,16 +14,22 @@ const whitelist = [
 ];
 
 // Configure CORS options
+
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*",
   credentials: true,
 };
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
 
 const app = express();
 
@@ -60,8 +66,3 @@ app.use("/api/v1/movies", movieRoutes);
 app.use("/api/v1/upload", uploadRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-// const corsOptions = {
-//   origin: "*",
-//   credentials: true,
-// };
