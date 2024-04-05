9bb75c2396d0b5448ff17319cf16381ea596e1f1
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-05 01:50:37 +0100

    jwt

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -22,7 +22,7 @@ const authenticate = asyncHandler(async (req, res, next) => {
     }
   } else {
     res.status(401);
-    throw new Error("Not Authorized, no token");
+    throw new Error("Not Authorized, no token Oga");
   }
 });
 
