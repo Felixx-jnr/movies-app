aa565ed5f30bf0222c8257b84bba6f9c3cac06cb
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-05 21:30:51 +0100

    work

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,16 @@ const connectDB = require("./config/db");
 
 // Configure CORS options
 
-const corsOptions = {
-  origin: "https://movieshq.onrender.com",
-  origin: "https://movieshq-api.onrender.com",
-  credentials: true,
-};
-
 // const corsOptions = {
-//   origin: "*",
+//   origin: "https://movieshq.onrender.com",
+//   origin: "https://movieshq-api.onrender.com",
 //   credentials: true,
 // };
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+};
 
 const app = express();
 
