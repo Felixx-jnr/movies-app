3fbbb42cd2ef3638a3f0cbd948b960496779a199
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-05 17:46:06 +0100

    auth

--- frontend/src/pages/Auth/PrivateRoute.jsx
+++ frontend/src/pages/Auth/PrivateRoute.jsx
@@ -3,8 +3,14 @@ import { useSelector } from "react-redux";
 
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
-
-  return userInfo ? <Outlet /> : <Navigate to="/login" />;
+  return userInfo ? (
+    <Outlet />
+  ) : (
+    <Navigate
+      to="/login"
+      replace
+    />
+  );
 };
 
 export default PrivateRoute;
