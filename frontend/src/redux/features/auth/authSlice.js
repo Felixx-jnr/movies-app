b7b624ce1a0f67e09f3a8eed4768d83c424f6a31
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-05 23:29:10 +0100

    auth

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -1,7 +1,41 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const userInfoFromLocalStorage = localStorage.getItem("userInfo");
+
+// const initialSlice = {
+//   userInfo: userInfoFromLocalStorage
+//     ? JSON.parse(userInfoFromLocalStorage)
+//     : null,
+// };
+
+// const authSlice = createSlice({
+//   name: "auth",
+//   initialState: initialSlice,
+//   reducers: {
+//     setCredentials: (state, action) => {
+//       state.userInfo = action.payload;
+//       localStorage.setItem("userInfo", JSON.stringify(action.payload));
+
+//       const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000;
+//       localStorage.setItem("expirationTime", expirationTime);
+//     },
+
+//     logout: (state) => {
+//       state.userInfo = null;
+//       localStorage.clear();
+//     },
+//   },
+// });
+
+// export const { setCredentials, logout } = authSlice.actions;
+// export default authSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialSlice = {
-  userInfo: localStorage.getItem("userInfo"),
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
 };
 
 const authSlice = createSlice({
