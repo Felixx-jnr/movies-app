227a20434fb54a0e8f64f11179c2fc7b6a319e2e
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-09 08:43:40 +0100

    main

--- frontend/src/pages/Admin/AllComment.jsx
+++ frontend/src/pages/Admin/AllComment.jsx
@@ -24,7 +24,7 @@ const AllComments = () => {
       {movie?.map((m) => (
         <section
           key={m._id}
-          className="flex flex-col justify-center items-center"
+          className="flex flex-col justify-center items-center my-4"
         >
           {m?.reviews.map((review) => (
             <div
@@ -32,7 +32,9 @@ const AllComments = () => {
               className="bg-[#1A1A1A] p-4 rounded-lg w-[90%] mt-[2rem]"
             >
               <div className="flex justify-between">
-                <strong className="text-[#B0B0B0]">{review.name}</strong>
+                <strong className="text-[#B0B0B0] capitalize">
+                  {review.name}
+                </strong>
                 <p className="text-[#B0B0B0]">
                   {review.createdAt.substring(0, 10)}
                 </p>
--- frontend/src/pages/Admin/Dashboard/Sidebar/Sidebar.jsx
+++ frontend/src/pages/Admin/Dashboard/Sidebar/Sidebar.jsx
@@ -5,15 +5,15 @@ const Sidebar = () => {
     <div className=" mt-5 ">
       <aside className="">
         <ul className="text-center">
-          <li className="inline-block mx-2 text-lg bg-gradient-to-b from-red-500 to-red-400 rounded-full">
+          <li className="inline-block m-2 p-1 text-lg bg-gradient-to-b from-red-500 to-red-400 rounded-full">
             <Link
               to="/admin/movies/dashboard"
-              className=" p-1 mb-3 hover:bg-gradient-to-b from-red-300 to-red-100 rounded-full"
+              className=" p-1 mb-3 "
             >
               Dashboard
             </Link>
           </li>
-          <li className="inline-block border  mx-2 text-lg hover:bg-gradient-to-b from-red-500 to-red-400 rounded-full">
+          <li className="inline-block m-2 border-none bg-neutral-700 p-1 mx-2 text-lg hover:bg-gradient-to-b from-red-500 to-red-400 rounded-full">
             <Link
               to="/admin/movies/create"
               className=" mx-0 p-1 mb-3"
@@ -21,7 +21,7 @@ const Sidebar = () => {
               Create Movie
             </Link>
           </li>
-          <li className="inline-block mx-2 text-lg hover:bg-gradient-to-b from-red-500 to-red-400 rounded-full">
+          <li className="inline-block m-2 text-lg border-none bg-neutral-700 p-1 hover:bg-gradient-to-b from-red-500 to-red-400 rounded-full">
             <Link
               to="/admin/movies/genre"
               className=" p-1 mb-3"
@@ -29,7 +29,7 @@ const Sidebar = () => {
               Create Genre
             </Link>
           </li>
-          <li className="inline-block mx-2 text-lg hover:bg-gradient-to-b from-red-500 to-red-400 rounded-full">
+          <li className="inline-block m-2 text-lg border-none p-1 bg-neutral-700 hover:bg-gradient-to-b from-red-500 to-red-400 rounded-full">
             <Link
               to="/admin/movies-list"
               className="p-1 mb-3"
@@ -37,10 +37,10 @@ const Sidebar = () => {
               Update Movie
             </Link>
           </li>
-          <li className="inline-block mx-2 text-lg hover:bg-gradient-to-b from-red-500 to-red-400 rounded-full">
+          <li className="inline-block m-2 text-lg border-none p-1 bg-neutral-700 hover:bg-gradient-to-b from-red-500 to-red-400 rounded-full">
             <Link
               to="/admin/movies/comments"
-              className=" inline p-1 mb-3"
+              className="p-1 mb-3"
             >
               Comments
             </Link>
