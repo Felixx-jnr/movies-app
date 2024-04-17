76ba398e0c126c42edb6a3fbe514db561ce664f3
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-17 13:24:38 +0100

    donesv

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = async()=>{
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log('successfully connected')
-  } catch(error){
-    console.log(`Error${error.message}`)
-    process.exit(1)
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("successfully connected");
+  } catch (error) {
+    console.log(`Error${error.message}`);
+    process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
