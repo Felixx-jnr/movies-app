0f23b073ee115c9482972558c07077a95ffee571
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-18 18:15:59 +0100

    errr

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://Deitycoder:codercoder@cluster0.y3q2atx.mongodb.net/MoviesHQ?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("successfully connected");
   } catch (error) {
     console.log(`Error${error.message}`);
