91f500d06907e147b430ce4aeaaa9248d60f82c7
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-18 08:22:16 +0100

    vercel

--- backend/index.js
+++ backend/index.js
@@ -1,72 +1,25 @@
-// //dependencies
-// const express = require("express");
-// const cookieParser = require("cookie-parser");
-// const dotenv = require("dotenv");
-// const path = require("path");
-// const cors = require("cors");
-
-// //Files
-// const connectDB = require("./config/db");
-
-// // Configure CORS options
-
-// const corsOptions = {
-//   origin: ["https://movieshq.onrender.com"],
-//   credentials: true,
-// };
-
-// const app = express();
-
-// app.use(cors(corsOptions));
-
-// dotenv.config();
-// connectDB();
-
-// //Routes
-// const userRoutes = require("./routes/userRoutes");
-// const genreRoutes = require("./routes/genreRoutes");
-// const movieRoutes = require("./routes/movieRoutes");
-// const uploadRoutes = require("./routes/uploadRoutes");
-
-// app.get("/", (req, res) => {
-//   res.send("MoviesHq API working");
-// });
-
-// //middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-
-// // Serve static files from the frontend build directory
-// app.use(express.static(path.join(__dirname, "frontend", "src", "assets")));
-
-// // Route for serving the index.html file
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
-// });
-
-// const PORT = process.env.PORT || 4000;
-
-// //Routes
-// app.use("/api/v1/users", userRoutes);
-// app.use("/api/v1/genres", genreRoutes);
-// app.use("/api/v1/movies", movieRoutes);
-// app.use("/api/v1/upload", uploadRoutes);
-
-// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
+//dependencies
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const path = require("path");
+const cors = require("cors");
 
 //Files
 const connectDB = require("./config/db");
 
-// Configure environment variables
-dotenv.config();
+// Configure CORS options
 
-// Connect to the database
+const corsOptions = {
+  origin: ["https://movieshq.onrender.com"],
+  credentials: true,
+};
+
+const app = express();
+
+app.use(cors(corsOptions));
+
+dotenv.config();
 connectDB();
 
 //Routes
@@ -75,28 +28,29 @@ const genreRoutes = require("./routes/genreRoutes");
 const movieRoutes = require("./routes/movieRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 
-// Create Express app
-const app = express();
+app.get("/", (req, res) => {
+  res.send("MoviesHq API working");
+});
 
-// Middleware
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Serve static files from the frontend build directory
-// Adjust the path according to your project structure
-app.use(express.static(path.join(__dirname, "frontend", "public")));
+app.use(express.static(path.join(__dirname, "frontend", "src", "assets")));
 
-// Routes
+// Route for serving the index.html file
 app.get("/", (req, res) => {
-  res.send("MoviesHq API working");
+  res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
 });
 
-// Use routes
+const PORT = process.env.PORT || 4000;
+
+//Routes
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/genres", genreRoutes);
 app.use("/api/v1/movies", movieRoutes);
 app.use("/api/v1/upload", uploadRoutes);
 
-// Export the Express app for Vercel
-module.exports = app;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
