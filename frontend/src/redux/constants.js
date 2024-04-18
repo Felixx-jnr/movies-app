27a8748e459a6b02549c16729255490ac68be750
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-18 22:29:54 +0100

    done

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://movieshq-api.onrender.com";
+export const BASE_URL = "https://movieshq.vercel.app";
 export const USERS_URL = "/api/v1/users";
 export const GENRE_URL = "/api/v1/genres";
 export const MOVIE_URL = "/api/v1/movies";
