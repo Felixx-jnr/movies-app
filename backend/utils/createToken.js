fa7f43cb680cad47716dd8aef8e5a5c5475afc08
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-21 23:03:18 +0100

    ver2

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === "production",
-    // sameSite: "None",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
