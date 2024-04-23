b93ebccb78fdfe339185a6d4961a6670a0627f16
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-23 09:12:47 +0100

    test3

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,27 +1,15 @@
-// const jwt = require("jsonwebtoken");
-
-// const generateToken = (res, userId) => {
-//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-//     expiresIn: "30d",
-//   });
-
-//   // Set JWT as an HTTP-Only Cookie
-//   res.cookie("jwt", token, {
-//     secure: true,
-//     sameSite: "None",
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//   });
-
-//   return token;
-// };
-
-// module.exports = generateToken;
-
 const jwt = require("jsonwebtoken");
 
-const generateToken = (userId) => {
+const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
+  });
+
+  // Set JWT as an HTTP-Only Cookie
+  res.cookie("jwt", token, {
+    secure: true,
+    sameSite: "None",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
   return token;
